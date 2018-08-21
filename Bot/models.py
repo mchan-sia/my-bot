ac361adf2115f5eae85dcf4315fdafae0b0c85ab
mchan-sia <42265069+mchan-sia@users.noreply.github.com>
2018-08-21 15:12:44 +0200

    Add files via upload

--- Bot/models.py
+++ Bot/models.py
@@ -8,3 +8,13 @@ class TAUX_ENR(models.Model):
     
     def __str__(self):
         return self.pays
+
+
+class OPTIM_ENR(models.Model):
+    pays = models.CharField(max_length=200,verbose_name ='pays',default='',primary_key=True)
+    optim = models.DecimalField (verbose_name ='optim', default=0,max_digits=10,decimal_places=5)
+      
+      
+    def __str__(self):
+        return self.pays
+
